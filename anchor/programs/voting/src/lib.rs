82150326ae61c8ddc4bfb2aecaf5e57e6ec4dc42
Your Name <suhasmashety@gmail.com>
2025-08-27 09:04:00 +0000

    completed contract

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -9,6 +9,7 @@ pub mod voting {
     pub fn initialize_poll(ctx: Context<InitializePoll>, poll_id: u64, poll_name: String, poll_description: String, start_time: u64, end_time: u64,  ) -> Result<()> {
         let poll = &mut ctx.accounts.poll_account;
         poll.poll_name = poll_name;
+        poll.poll_id = poll_id;
         poll.poll_description = poll_description;
         poll.poll_voting_start = start_time;
         poll.poll_voting_end = end_time;
@@ -16,13 +17,53 @@ pub mod voting {
         Ok(())
     }
 
-    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, candidate_name: String, poll_id: u64) -> Result<()> {
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, candidate_name: String, _poll_id: u64) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate_account;
         candidate.candidate_name = candidate_name;
         ctx.accounts.poll_account.poll_option_index += 1; 
 
         Ok(())
     }
+}
+
+    pub fn vote(ctx: Context<InitializeVote>, _poll_id: u64, _candidate_name: String) -> Result<()> {
+        let candidate_account = &mut ctx.accounts.candidate_account;
+        let poll_account = &mut ctx.accounts.poll_account;
+        let present_time = Clock::get()?.unix_timestamp;
+
+        require!(
+            present_time >= poll_account.poll_voting_start as i64,
+            ErrorCode::VotingNotStarted
+        );
+        require!(
+            present_time < poll_account.poll_voting_end as i64, 
+            ErrorCode::VotingEnded
+        );
+
+        candidate_account.candidate_votes = candidate_account.candidate_votes.checked_add(1).ok_or(ErrorCode::VotingOverFlow)?;
+        Ok(())
+    }
+
+#[derive(Accounts)]
+#[instruction(poll_id:u64, candidate_name: String)]
+pub struct InitializeVote<'info> {
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"candidate".as_ref(), poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes().as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+
+    pub system_program: Program<'info, System>
 }
 
 #[derive(Accounts)]
@@ -54,7 +95,7 @@ pub struct InitializeCandidate<'info> {
         init,
         payer = signer,
         space = 8 + CandidateAccount::INIT_SPACE,
-        seeds = [b"candidate".as_ref(), poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+        seeds = [b"candidate".as_ref(), poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes().as_ref()],
         bump
     )]
 
@@ -70,6 +111,7 @@ pub struct PollAccount {
     pub poll_name: String,
     #[max_len(250)]
     pub poll_description: String,
+    pub poll_id: u64,
     pub poll_voting_start: u64,
     pub poll_voting_end: u64,
     pub poll_option_index: u64
@@ -83,3 +125,12 @@ pub struct CandidateAccount {
     pub candidate_votes: u64
 }
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Voting has not started yet")]
+    VotingNotStarted,
+    #[msg("Voting has ended")]
+    VotingEnded,
+    #[msg("Voting has reached MAX")]
+    VotingOverFlow,
+}
